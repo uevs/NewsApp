2ec41088bb2e4271b3d46f092a4e894b6ef5e66e
uevs <36189306+uevs@users.noreply.github.com>
2022-11-17 10:37:02 +0100

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by leonardo on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
