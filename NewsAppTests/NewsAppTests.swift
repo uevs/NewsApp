80ce26c09463df6668ccab538fc0ff6bdb08956a
uevs <36189306+uevs@users.noreply.github.com>
2022-11-23 19:39:48 +0100

    Added UI tests to check card behaviour

--- NewsAppTests/NewsAppTests.swift
+++ NewsAppTests/NewsAppTests.swift
@@ -11,13 +11,6 @@ import Combine
 
 final class NewsAppTests: XCTestCase {
 
-//    override func setUpWithError() throws {
-//    }
-
-//    override func tearDownWithError() throws {
-//
-//    }
-
     func testModelComputedProperties() throws {
         /// Tests the computed properties of the data model
 
@@ -35,16 +28,15 @@ final class NewsAppTests: XCTestCase {
 
         XCTAssertTrue(news.imageURL != URL(string: "https://dummyimage.com/600"), "Couldn't unwrap the image url")
     }
-    
+
     func testNetworkManager() throws {
         /// Tests if the Network Manager can get data from the api
-    
+
         let url = URL(string: "https://run.mocky.io/v3/de42e6d9-2d03-40e2-a426-8953c7c94fb8")!
         var result: [News] = []
         var cancellables = Set<AnyCancellable>()
         let expectation = self.expectation(description: "received")
 
-        
         NetworkManager.getData(url: url)
             .decode(type: [News].self, decoder: JSONDecoder())
             .receive(on: DispatchQueue.main)
@@ -60,15 +52,14 @@ final class NewsAppTests: XCTestCase {
                 expectation.fulfill()
             }
             .store(in: &cancellables)
-        
+
         waitForExpectations(timeout: 5)
         XCTAssertTrue(!result.isEmpty, "NetworkManager published an empty array.")
     }
-    
-    
+
     func testImageLoader() throws {
         /// Tests if the ImageLoader publishes and image
-        
+
         let imageLoader = ImageLoader(url: URL(string: "https://dummyimage.com/367x809.png/ff4444/ffffff")!, id: 17)
         let expectation = self.expectation(description: "received")
         var image: UIImage?
@@ -79,25 +70,22 @@ final class NewsAppTests: XCTestCase {
                 expectation.fulfill()
             }
         })
-        
+
         waitForExpectations(timeout: 5)
         XCTAssertTrue(image != nil, "ImageLoader didn't publish an image")
-        
+
         let url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first?.appendingPathComponent("images").appendingPathComponent("17.png")
         try FileManager.default.removeItem(atPath: url!.path)
     }
-    
-    
+
     func testViewModel() throws {
         /// Tests if the view models correctly fetches data from the internet
-        
+
         let viewModel = DataStore()
         let testNews = News(id: 17, title: "Decentralized transitional moderator", description: "non mi integer ac neque duis bibendum morbi non quam nec dui luctus rutrum nulla tellus in sagittis dui vel nisl duis ac nibh fusce lacus purus aliquet at feugiat non pretium quis lectus suspendisse potenti in eleifend quam a odio in hac habitasse platea dictumst maecenas ut massa quis augue luctus tincidunt nulla mollis molestie lorem quisque ut erat curabitur gravida nisi at nibh in hac habitasse platea dictumst aliquam augue quam sollicitudin vitae consectetuer eget rutrum at lorem integer tincidunt ante vel ipsum praesent blandit lacinia erat vestibulum sed magna at nunc commodo placerat", release_date: "5/25/2018", author: "kpittmang@behance.net", image: "http://dummyimage.com/367x809.png/ff4444/ffffff")
-    
+
         XCTAssertTrue(!viewModel.news.isEmpty, "News array is empty")
         XCTAssertTrue(viewModel.news.first!.date > viewModel.news.last!.date, "News are not sorted by date")
         XCTAssertTrue(viewModel.news.first(where: { $0.id == 17 }) == testNews, "Incorrect News at id 17")
     }
-    
-
 }
--- NewsAppUITests/NewsAppUITests.swift
+++ NewsAppUITests/NewsAppUITests.swift
@@ -10,32 +10,52 @@ import XCTest
 final class NewsAppUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testDetailView() throws {
+        /// Tests if a card can be opened and if there's correct information inside
+
+        let app = XCUIApplication()
+        app.launch()
+        app.scrollViews.otherElements.staticTexts["Fully-configurable zero tolerance budgetary management"].tap()
+
+        let author = app.scrollViews.otherElements.staticTexts["Author: dyakuntsov1d@mac.com"]
+        let date = app.scrollViews.otherElements.staticTexts["Tue, Mar 10, '20"]
+
+        XCTAssertTrue(author.exists)
+        XCTAssertTrue(date.exists)
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testSwipeAndOpen() throws {
+        /// Tests a sequence of swipes and then a card opening
+
         let app = XCUIApplication()
         app.launch()
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let elementsQuery = app.scrollViews.otherElements
+        elementsQuery.staticTexts["Fully-configurable zero tolerance budgetary management"].swipeUp()
+        elementsQuery.staticTexts["Optional mobile contingency"].tap()
+
+        let author = app.scrollViews.otherElements.staticTexts["Author: gleggis1a@rambler.ru"]
+        let date = app.scrollViews.otherElements.staticTexts["Fri, Oct 25, '19"]
+
+        XCTAssertTrue(author.exists)
+        XCTAssertTrue(date.exists)
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    func testCardOpenAndClose() throws {
+        /// Tests the opening and then closing of a card
+
+        let app = XCUIApplication()
+        app.launch()
+
+        app.scrollViews.otherElements.staticTexts["Inverse asynchronous projection"].tap()
+        app.windows.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .button)["x.circle.fill"].tap()
+
+        let card = app.scrollViews.otherElements.staticTexts["Reactive well-modulated alliance"]
+
+        XCTAssertTrue(card.exists)
+
     }
 }
